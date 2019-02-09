aed0daa4fdae0e372f722a54b75f03b87d930478
latofat <latofat666@mail.ru>
2019-02-09 23:10:03 +0500

    Added Registration Page and Sing in page

--- app/src/main/java/corp/king/booksapp/presentation/StartActivity.java
+++ app/src/main/java/corp/king/booksapp/presentation/StartActivity.java
@@ -10,6 +10,6 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_sign_in);
     }
 }
