2e5bacece64ec0438a6a320e9ebd109a29567f73
Shakhzodbek Isomiddinov <shakhzodbek.isomiddinov@gmail.com>
2019-02-12 16:29:12 +0500

    Refactor MVP. Linking Registration and Sign In pages

---
+++ app/src/main/java/corp/king/booksapp/presentation/callbacks/StartCallback.java
@@ -0,0 +1,9 @@
+package corp.king.booksapp.presentation.callbacks;
+
+import android.app.Dialog;
+import android.view.View;
+
+public interface StartCallback {
+    void onGetStartedClicked(View view);
+    void onRegistrationClicked(View view);
+}
