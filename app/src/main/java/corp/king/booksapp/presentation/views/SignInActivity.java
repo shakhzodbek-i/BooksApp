af0a0bf2aac66e9e6633899e0cd370f618002db2
latofat <latofat666@mail.ru>
2019-04-10 12:29:01 +0500

    Google Sign in Added

--- app/src/main/java/corp/king/booksapp/presentation/views/SignInActivity.java
+++ app/src/main/java/corp/king/booksapp/presentation/views/SignInActivity.java
@@ -1,99 +1,132 @@
 package corp.king.booksapp.presentation.views;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import corp.king.booksapp.R;
-import corp.king.booksapp.presentation.presenters.SignInPresenterImpl;
-import corp.king.booksapp.presentation.presenters.interfaces.ISignInPresenter;
-import corp.king.booksapp.presentation.views.interfaces.ISignInView;
 
-public class SignInActivity extends AppCompatActivity implements ISignInView {
-
-   private Button signIn;
-   private EditText email;
-   private EditText password;
-   private ProgressBar progressBar;
-    private ISignInPresenter mPresenter;
-
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+    private Button signOut;
+    private SignInButton signIn;
+    private TextView name, email;
+    private ImageView prof_Pic;
+    private ConstraintLayout profile_sec;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE=9001;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-
         initView();
 
-        mPresenter = new SignInPresenterImpl(this);
+        signOut.setOnClickListener(this);
+        signIn.setOnClickListener(this);
 
-        signIn.setOnClickListener(view -> {
-
-             String email= this.email.getText().toString().trim();
-             String password= this.password.getText().toString().trim();
-
-            if(email.isEmpty()){
-                this.email.setError("Email is required");
-                this.email.requestFocus();
-                return;
-            }
-
-            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                this.email.setError("Please enter a valid email");
-                this.email.requestFocus();
-                return;
-            }
-
-            if(password.length()<6){
-                this.password.setError("Minimum length of password should be 6");
-                this.password.requestFocus();
-                return;
-            }
-
-            if(password.isEmpty()){
-                this.password.setError("Password is required");
-                this.password.requestFocus();
-                return;
-            }
-            showProgress();
-            mPresenter.signIn(email, password);
-                    });
+        profile_sec.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
     }
 
     private void initView() {
-        signIn= findViewById(R.id.sign_in);
-        email = findViewById(R.id.login);
-        password= findViewById(R.id.password);
-        progressBar= findViewById(R.id.prog_bar);
+        profile_sec = findViewById(R.id.Constraint);
+        signOut = findViewById(R.id.btn_logout);
+        signIn = findViewById(R.id.google_log_in);
+        name = findViewById(R.id.username);
+        email = findViewById(R.id.useremail);
+        prof_Pic = findViewById(R.id.prif_pic);
     }
 
     @Override
-    public void navigateToMainPage() {
-        Intent intent=new Intent(SignInActivity.this, MainActivity.class );
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+    public void onClick(View view) {
+    switch (view.getId()){
+        case R.id.google_log_in:
+            signIn();
+            break;
+
+        case R.id.btn_logout:
+            signOut();
+            break;
+    }
     }
 
     @Override
-    public void showProgress() {
-        progressBar.setVisibility(View.VISIBLE);
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void signIn(){
+        Intent intent =Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODE);
+    }
+
+    private void signOut(){
+
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+
+    }
+
+    private void handleResult(GoogleSignInResult result){
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            String this_name=account.getDisplayName();
+            String this_email=account.getEmail();
+            Uri img_url=account.getPhotoUrl();
+            name.setText(this_name);
+            email.setText(this_email);
+            Glide.with(this).load(img_url).fitCenter().placeholder(R.drawable.user).into(prof_Pic) ;
+            updateUI(true);
+        }
+        else{
+            updateUI(false);
+        }
+
+    }
+
+    private void updateUI(boolean idLogin){
+        if(idLogin){
+            profile_sec.setVisibility(View.VISIBLE);
+            signIn.setVisibility(View.GONE);
+        }
+        else{
+            profile_sec.setVisibility(View.GONE);
+            signIn.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
-    public void hideProgress() {
-        progressBar.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void showError(String msg) {
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQ_CODE){
+            GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
     }
 }
